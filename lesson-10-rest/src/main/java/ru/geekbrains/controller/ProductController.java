9a0cd31cd0138c05fe1c0794fe688103796d9a51
Ivan <reinkarnaci9@gmail.com>
2021-03-06 21:00:12 +0300

    секьюрность для REST

--- lesson-10-rest/src/main/java/ru/geekbrains/controller/ProductController.java
+++ lesson-10-rest/src/main/java/ru/geekbrains/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -58,7 +59,7 @@ public class ProductController {
         return "artShop";
     }
 
-
+    @Secured({"ADMIN"})
     @GetMapping("/{id}")
     public String editPage(@PathVariable("id") Long id, Model model) {
         logger.info("Edit page for products id {} requested", id);
@@ -67,6 +68,7 @@ public class ProductController {
         return "product_form";
     }
 
+    @Secured({"ADMIN"})
     @GetMapping("/createProduct")
     public String addProduct(Model model) {
         logger.info("Create new Product");
@@ -75,7 +77,7 @@ public class ProductController {
         return "product_form";
     }
 
-
+    @Secured({"ADMIN"})
     @DeleteMapping("/{id}")
     public String deleteProduct(@PathVariable("id") Long id) {
         logger.info("Delete products with id = {}", id);
@@ -84,7 +86,7 @@ public class ProductController {
         return "redirect:/artshop";
     }
 
-
+    @Secured({"ADMIN"})
     @PostMapping("/update")
     public String updateProduct(@Valid ProductRepr productRepr, BindingResult result) {
         logger.info("Update endpoint requested");
@@ -106,7 +108,7 @@ public class ProductController {
         return modelAndView;
     }
 
-
+    @Secured({"GUEST"})
     @GetMapping("/basket")
     public String getBasket(Model model) {
         List<BasketProduct> basketProducts;
@@ -116,7 +118,7 @@ public class ProductController {
         return "basket";
 
     }
-
+    @Secured({"GUEST"})
     @GetMapping("/basket/add/{id}")
     public String addProductToBasket(@PathVariable("id") Long id, Model model) {
 
@@ -126,6 +128,7 @@ public class ProductController {
 
     }
 
+    @Secured({"GUEST"})
     @DeleteMapping("/basket/remove/{id}")
     public String deleteBasketProduct(@PathVariable("id") Long id) {
 
